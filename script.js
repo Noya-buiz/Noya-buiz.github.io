8ce87fba4e08bd849524ed869be7796aefd92bcf
no <anode.buiz@proton.me>
2026-05-17 20:18:12 +0200

    update site 2026

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("clickButton").addEventListener("click", function() {
-    document.getElementById("message").textContent = "Danke fürs Klicken!";
+    document.getElementById("message").textContent = "Thank you for clicking!";
 });
